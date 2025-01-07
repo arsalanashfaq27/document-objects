458c30cf32833c5999090ef5712c379da7ae60ec
arsalanashfaq27 <arsalanashfaq27@gmail.com>
2025-01-07 12:44:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const marksheetConainer = document.getElementById('marksheet');
+marksheetConainer.style.border = '5px solid green'
+marksheetConainer.style.width = '80%'
+marksheetConainer.style.height = '250px'
+
+const firstname = document.createElement('div')
+let Name1 = Prompt('What is your Name')
+firstname.innerHTML = ${Name1};
+
+
+
+const marksheet = (totalmarks,obtmarks)=>{
+    return obtMarks/totalMarks*100
+}
+// const hello = document.getElementById('greeting');
+
+// const name = prompt('What is your name?');
+
+// hello.innerHTML = `Hello, <em>${name}!</em>`;
+
+// const name = 'dfsdfs'
+// const bioData = {
+//     name:"ALi",
+//     address:{
+//         streetNo:""
+//     },
+//     test:function() {console.log(this.address)
+
+// }
+// console
+
+//const hello = document.getElementById('greeting');
+
+
+// const items = document.getElementsByTagName('li');
+// const fruit = document.getElementsByClassName('fruit');
+
+
+// const food = document.querySelector('#food');
+
+// const apple = food.children[0];
+// const banana = food.children[1];
+// const carrot = food.children[2];
+
+
+// const melon = document.createElement('li');
+// melon.innerHTML = `<span> </span>Melon`;
+
+// food.appendChild(melon)
+// const name = prompt('What is your name?');
+
+
+// hello.innerHTML = `Hello, <em>${name}!</em>`;
+
